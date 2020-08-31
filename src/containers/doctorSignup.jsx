767f2057496174a34311176ecad9479a57148071
shres-ruby <shres.ruby@gmail.com>
2020-08-31 12:09:06 +0545

    feat: enable doctors to sign up

--- src/containers/doctorSignup.jsx
+++ src/containers/doctorSignup.jsx
@@ -1,157 +1,152 @@
 import React from 'react';
-import { Form, Input, Button } from 'antd';
-import { connect } from 'react-redux';
-import * as actions from '../store/actions/auth';
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment
+} from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
 
 
-const RegistrationForm = (props) => {
-  const [form] = Form.useForm();
-
-  const onFinish = values => {
-    console.log('Received values of form: ', values);
-    props.onAuth(values.email, values.password, values.confirm,
-        values.firstname, values.lastname, values.phone, values.specialty,
-        values.availability)
-    props.history.push('/');
+class RegistrationForm extends React.Component {
+  state = {
+    info: {
+    email: "",
+    password: "",
+    password2: "",
+    first_name: "",
+    last_name: "",
+    phone: "",
+    specialty: "",
+    availability: ""
+  }
   };
-  
-  return (
-    <Form
-      form={form}
-      name="register"
-      onFinish={onFinish}
-      scrollToFirstError
-    >
-      <Form.Item
-        name="email"
-        label="E-mail"
-        rules={[
-          {
-            type: 'email',
-            message: 'The input is not valid E-mail!',
-          },
-          {
-            required: true,
-            message: 'Please input your E-mail!',
-          },
-        ]}
+
+  handleSubmit = e => {
+    e.preventDefault();
+    fetch('http://127.0.0.1:8000/api/doctorsignup/', {
+      method : 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(this.state.info)
+    })
+    .then( data => data.json())
+    .then(
+      data => {
+        console.log(data.token);
+      }
+    ).catch( error => console.log(error));
+    this.props.history.push('login/')
+  };
+
+  handleChange = e => {
+    const data = this.state.info;
+    data[e.target.name] = e.target.value;
+    this.setState({info: data});
+  };
+
+  render() {
+    return (
+      <Grid
+        textAlign="center"
+        style={{ height: "100vh" }}
+        verticalAlign="middle"
       >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name="confirm"
-        label="Confirm Password"
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Please confirm your password!',
-          },
-          ({ getFieldValue }) => ({
-            validator(rule, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-
-              return Promise.reject('The two passwords that you entered do not match!');
-            },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name="firstname"
-        label="First Name"
-        rules={[
-          {
-            required: true,
-            message: 'Enter your first name',
-            whitespace: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="lastname"
-        label="Last Name"
-        rules={[
-          {
-            required: true,
-            message: 'Enter your last name',
-            whitespace: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="phone"
-        label="Phone Number"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your phone number!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-        <Form.Item
-          name="specialty"
-          label="Specialty"
-          rules={[{ required: true, message: 'Please enter your specialty', }]}
-        >
-        <Input />
-        </Form.Item> 
-      <Form.Item
-        name="availability"
-        label="Availability"
-        rules={[
-          {
-            required: true,
-            message: 'Please enter your availability',
-          },
-        ]}
-      >
-      <Input />
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Register
-        </Button>
-      </Form.Item>
-    </Form>
-  );
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onAuth: (email, password1, password2, firstname, lastname, phone, specialty, availability) => 
-    dispatch(actions.authSignup(email, password1, password2, firstname, lastname, phone, specialty, availability))
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" color="teal" textAlign="center">
+            Doctor Registration
+          </Header>
+          <React.Fragment>
+            <Form size="large">
+              <Segment stacked>
+                <Form.Input
+                  onChange={this.handleChange}
+                  value={this.state.info.email}
+                  name="email"
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Email"
+                />
+                <Form.Input
+                  onChange={this.handleChange}
+                  fluid
+                  value={this.state.info.password}
+                  name="password"
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                />
+                <Form.Input
+                  onChange={this.handleChange}
+                  fluid
+                  value={this.state.info.password2}
+                  name="password2"
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Confirm password"
+                  type="password"
+                />
+                <Form.Input
+                  onChange={this.handleChange}
+                  value={this.state.info.first_name}
+                  name="first_name"
+                  fluid
+                  iconPosition="left"
+                  placeholder="First Name"
+                />
+                <Form.Input
+                  onChange={this.handleChange}
+                  value={this.state.info.last_name}
+                  name="last_name"
+                  fluid
+                  iconPosition="left"
+                  placeholder="Last Name"
+                />
+                <Form.Input
+                  onChange={this.handleChange}
+                  value={this.state.info.phone}
+                  name="phone"
+                  fluid
+                  iconPosition="left"
+                  placeholder="Phone"
+                />
+                <Form.Input
+                  onChange={this.handleChange}
+                  value={this.state.info.specialty}
+                  name="specialty"
+                  fluid
+                  iconPosition="left"
+                  placeholder="Specialty"
+                />
+                <Form.Input
+                  onChange={this.handleChange}
+                  value={this.state.info.availability}
+                  name="availability"
+                  fluid
+                  iconPosition="left"
+                  placeholder="Availability"
+                />
+                <Button
+                  color="teal"
+                  fluid
+                  size="large"
+                  onClick = {this.handleSubmit}
+                >
+                  Signup
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
+              Already have an account? <NavLink to="/login">Login</NavLink>
+            </Message>
+          </React.Fragment>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
-export default connect(null, mapDispatchToProps)(RegistrationForm);
+export default RegistrationForm;
